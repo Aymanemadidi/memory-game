83973498efce2c1bdb383b721b87cf6ee011cc0d
Aymane Elmadidi <aymanelmadidi@gmail.com>
2022-05-30 15:34:09 +0100

    random colors working

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 
 import "./App.css";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function App() {
   const [boxArr, setBoxArr] = useState(
-    Array.from({ length: 18 }, (_, i) => i + 1)
+    Array.from({ length: 9 }, (_, i) => i + 1) // 9 -12 -15 - 18
   );
   const [selectedBoxes, setSelectedBoxes] = useState([]);
+  const [flashedItems, setFlashedItems] = useState([]);
+  const [toComapre, setToCompare] = useState([]);
 
   function handleSelectBtn(i) {
     if (selectedBoxes.includes(i)) {
@@ -19,24 +25,47 @@ function App() {
       setSelectedBoxes([...selectedBoxes, i]);
       console.log(selectedBoxes);
     }
-    // console.log("i", i);
-    // const arrCopy = [...selectedBoxes];
-    // if (arrCopy.includes(i)) {
-    //   const index = arrCopy.findIndex(() => i);
-    //   arrCopy.splice(index, 1);
-    //   console.log(arrCopy);
-    // }
   }
+
+  const arr = [];
+  useEffect(() => {
+    if (arr.length < 3) {
+      for (let i = 0; i < 3; i++) {
+        arr.push(getRandomInt(boxArr.length));
+      }
+    }
+    setToCompare(arr);
+    console.log(arr);
+  }, []);
+
+  useEffect(() => {
+    let i = 0;
+    const flashInt = setInterval(() => {
+      if (i == 4) {
+        setFlashedItems([]);
+        clearInterval(flashInt);
+        return;
+      }
+      setFlashedItems([arr[i]]);
+      // setToCompare([i + 1]);
+      i++;
+    }, 1000);
+    return () => {
+      clearInterval(flashInt);
+    };
+  }, [setFlashedItems]);
 
   return (
     <div className="App text-center">
       <h1 className="text-indigo-600 text-2xl font-semibold">Hello from App</h1>
-      <div className="grid gap-5 mt-10 mx-10 grid-cols-3">
+      <div className={`grid gap-5 mt-10 mx-10 grid-cols-${boxArr.length / 3}`}>
         {boxArr.map((b, i) => {
           return (
             <button
               className={`px-5 py-12 bg-slate-300 rounded-md text-slate-900 
-              ${selectedBoxes.includes(i) ? "bg-slate-700" : ""}`}
+              ${selectedBoxes.includes(i) ? "bg-slate-700" : ""}${
+                flashedItems.includes(i) ? "bg-indigo-500" : ""
+              }`}
               key={b + i}
               onClick={() => handleSelectBtn(i)}
             >
