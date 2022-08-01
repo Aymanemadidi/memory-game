aaf445eab1f028cfe35e083608da1693eddf3d9c
Aymane <pc@pcs-MacBook-Air.local>
2022-08-02 00:01:03 +0100

    rewrote it in a much cleaner way now its working

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,186 +1,116 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-import { useState, useEffect, useLayoutEffect } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max);
+}
+
+function generateTen() {
+	let result = [];
+	let temp;
+	for (let i = 0; i < 11; i++) {
+		temp = getRandomInt(15);
+		while (result.includes(temp)) {
+			temp = getRandomInt(15);
+		}
+		result.push(temp);
+	}
+	return result;
 }
 
 function App() {
-  const [boxArr] = useState(
-    Array.from({ length: 16 }, (_, i) => i + 1) // 9 -12 -15 - 18
-  );
-  const [selectedBoxes, setSelectedBoxes] = useState([]);
-  const [flashedItems, setFlashedItems] = useState([]);
-  const [toComapre, setToCompare] = useState([]);
-  const [nope, setNope] = useState(false);
-  const [started, setStarted] = useState(false);
-  const [enabled, setEnabled] = useState(true);
-  const [switchFill, setSwitchFill] = useState(true);
-  const [win, setWin] = useState(false);
-  // const [switch, setSwitch] = useState(false);
-  // const [counter, setCounter] = useState(10);
-  // const [firstRender, setFirstRender] = useState(true);
+	const [boxArr] = useState(
+		() => Array.from({ length: 16 }, (_, i) => i + 1) // 9 -12 -15 - 18
+	);
+	const [selectedBoxes, setSelectedBoxes] = useState([]);
+	const [toFlashItem, setToFlashItem] = useState();
+	const [flashedItems, setFlashedItems] = useState([150]);
 
-  function handleSelectBtn(i) {
-    if (selectedBoxes.includes(i)) {
-      const selectedCopy = [...selectedBoxes];
-      const index = selectedCopy.findIndex((elem) => elem === i);
-      selectedCopy.splice(index, 1);
-      setSelectedBoxes([...selectedCopy]);
-    } else {
-      setSelectedBoxes([...selectedBoxes, i]);
-      // console.log(selectedBoxes);
-    }
-  }
+	function arrayEquals(a, b) {
+		return (
+			Array.isArray(a) &&
+			Array.isArray(b) &&
+			a.length === b.length &&
+			a.every((val, index) => val === b[index])
+		);
+	}
 
-  const arr = [];
-  useEffect(() => {
-    if (arr.length < 7) {
-      for (let i = 0; i < 7; i++) {
-        let toPush = getRandomInt(boxArr.length);
-        while (arr.includes(toPush)) {
-          toPush = getRandomInt(boxArr.length);
-        }
-        arr.push(toPush);
-      }
-    }
-    setToCompare(arr);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	function handleSelectBtn(i) {
+		if (selectedBoxes.includes(i)) {
+			const selectedCopy = [...selectedBoxes];
+			const index = selectedCopy.findIndex((elem) => elem === i);
+			selectedCopy.splice(index, 1);
+			setSelectedBoxes([...selectedCopy]);
+		} else {
+			setSelectedBoxes([...selectedBoxes, i]);
+			// console.log(selectedBoxes);
+		}
+	}
 
-  function fillArr() {
-    if (arr.length < 7) {
-      for (let i = 0; i < 7; i++) {
-        let toPush = getRandomInt(boxArr.length);
-        while (arr.includes(toPush)) {
-          toPush = getRandomInt(boxArr.length);
-        }
-        arr.push(toPush);
-      }
-    }
-    setToCompare(arr);
-  }
+	function startGame() {
+		let toFlashArr = generateTen();
+		let i = 0;
+		let tempArr = [];
+		let interval = setInterval(() => {
+			setToFlashItem(toFlashArr[i]);
+			tempArr.push(toFlashArr[i]);
+			i++;
+			if (i == 12) {
+				tempArr.pop();
+				setFlashedItems(tempArr);
+				clearInterval(interval);
+				setToFlashItem(null);
+			}
+		}, 500);
+	}
 
-  useLayoutEffect(() => {
-    fillArr();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [switchFill]);
+	useEffect(() => {
+		console.log("I enter check effect");
+		if (arrayEquals(flashedItems, selectedBoxes)) {
+			alert("You Won!");
+			setSelectedBoxes([]);
+			setFlashedItems([150]);
+		}
+	}, [flashedItems, selectedBoxes]);
 
-  useEffect(() => {
-    // console.log("SELECTED", selectedBoxes);
-    // console.log("TOCOMPARE", toComapre);
-    if (selectedBoxes.length > 0) {
-      // console.log("check");
-      for (let i = 0; i < selectedBoxes.length; i++) {
-        if (toComapre[i] !== selectedBoxes[i] && !win) {
-          // console.log(false);
-          alert("NOPE");
-          setWin(false);
-          setSelectedBoxes([]);
-          return;
-        }
-      }
-    }
-    //console.log("length", selectedBoxes.length);
-    if (selectedBoxes.length === 7) {
-      alert("you won");
-      setWin(true);
-      setSelectedBoxes([]);
-      setEnabled(true);
-      setSwitchFill(!switchFill);
-      return;
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedBoxes, toComapre]);
-
-  useEffect(() => {
-    if (nope) {
-      alert("Nope");
-    }
-  }, [nope]);
-
-  // function checkWin(first, second) {
-  //   if (win) {
-  //     console.log("From func checkWin");
-  //     clearInterval(first);
-  //     clearInterval(second);
-  //   }
-  // }
-
-  useEffect(() => {
-    if (!win) {
-      let i = 0;
-      setInterval(() => {
-        if (i === 8) {
-          setFlashedItems([]);
-          return;
-        }
-        setFlashedItems([toComapre[i]]);
-        i++;
-      }, 1000);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [started]);
-
-  function startGame() {
-    let j = 0;
-    setWin(false);
-    if (enabled) setEnabled(false);
-    const timeout = setTimeout(() => {
-      if (j === 10) {
-        setNope(true);
-        setSwitchFill(!switchFill);
-        setEnabled(true);
-        // checkWin(flashInt, timeout);
-        console.log("cleared");
-        clearInterval(timeout);
-      }
-      j++;
-      // console.log(j);
-    }, 1000);
-    // if (nope) setNope(false);
-    setSelectedBoxes([]);
-    console.log("SYNC", toComapre);
-    setStarted(!started);
-  }
-
-  return (
-    <div className="App text-center">
-      <h1 className="text-indigo-600 text-2xl font-semibold mt-4">
-        Memory Game
-      </h1>
-      <div className={`grid gap-5 mt-10 mx-10 grid-cols-4`}>
-        {boxArr.map((b, i) => {
-          return (
-            <button
-              className={`px-5 py-12 bg-slate-300 rounded-md text-slate-900 
+	return (
+		<div className="App text-center">
+			<h1 className="text-indigo-600 text-2xl font-semibold mt-4">
+				Memory Game
+			</h1>
+			<div className={`grid gap-5 mt-10 mx-10 grid-cols-4`}>
+				{boxArr.map((box, i) => {
+					return (
+						<button
+							className={`px-5 py-12 bg-slate-300 rounded-md text-slate-900 
               ${
-                selectedBoxes.includes(i) ? "bg-slate-700 text-slate-700" : ""
-              }${
-                flashedItems.includes(i) ? "bg-indigo-500 text-indigo-500" : ""
-              }`}
-              key={b + i}
-              onClick={() => handleSelectBtn(i)}
-            >
-              {b - 1}
-            </button>
-          );
-        })}
-      </div>
-      <div className="text-center mt-5 ">
-        <button
-          className="bg-indigo-500 text-white p-3 rounded-md hover:bg-indigo-400 disabled:bg-indigo-300"
-          onClick={() => startGame()}
-          disabled={!enabled}
-        >
-          Start Game
-        </button>
-        {/* <p>{`time left ${counter} seconds`}</p> */}
-      </div>
-    </div>
-  );
+								selectedBoxes.includes(i) ? "bg-slate-700 text-slate-700" : ""
+							}${toFlashItem == i ? "bg-indigo-500 text-indigo-500" : ""}`}
+							key={box + i}
+							onClick={() => handleSelectBtn(i)}
+							disabled={selectedBoxes.includes(i)}
+						>
+							{box - 1}
+						</button>
+					);
+				})}
+			</div>
+			<div className="text-center mt-5 ">
+				<button
+					className="bg-indigo-500 text-white p-3 rounded-md hover:bg-indigo-400 disabled:bg-indigo-300"
+					onClick={() => startGame()}
+					// disabled={!enabled}
+				>
+					Start Game
+				</button>
+				{/* <p>{`time left ${counter} seconds`}</p> */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
